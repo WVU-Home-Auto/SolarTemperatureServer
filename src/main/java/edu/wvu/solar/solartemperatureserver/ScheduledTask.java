bd2f9012a0c835ce8229f035ddf055ebbffdbd91
Timmy <timjimski@gmail.com>
2015-08-31 16:14:01 -0400

    Added code to poll the electric imp server.

--- src/main/java/edu/wvu/solar/solartemperatureserver/ScheduledTask.java
+++ src/main/java/edu/wvu/solar/solartemperatureserver/ScheduledTask.java
@@ -1,14 +1,22 @@
 package edu.wvu.solar.solartemperatureserver;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 @Component
 @PropertySource("classpath:config.properties")
 public class ScheduledTask {
 	
+	private static final Logger logger = LogManager.getLogger(ScheduledTask.class);
+
 	/**
 	 * This method will run according to the cron set in the config file.
 	 * (It will probably run every minute or every 5 minutes or something.)
@@ -19,7 +27,15 @@ public class ScheduledTask {
 	 */
 	@Scheduled(cron = "${tempCheckCron}")
 	public void update(){
-		System.out.println(new DateTime().toString());
+		String[] sensors = TemperatureLog.getSensorNames();
+		for(String s : sensors){
+			try {
+				HttpResponse<String> response = Unirest.get(TemperatureLog.getUrl(s)).asString();
+				System.out.println(response.getBody());
+			} catch (UnirestException e) {
+				logger.error("Error retrieving temperature from " + s + ": " + e.getMessage());
+			}
+		}
 	}
 	
 }
